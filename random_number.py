5f3672c66017c71fb8a6c27afaa9c0d9582a7c53
hojiii <101567172+hojiii@users.noreply.github.com>
2022-04-07 16:51:06 +0900

    랜덤게임
    수정 1

--- random_number.py
+++ random_number.py
@@ -11,12 +11,12 @@ def check_answer(guess, answer, turns):
         print(f"{guess}는 정답보다 작습니다.")
         return turns-1
     else:
-        print("축하합니다 정답입니다.")
+        return -1
     
 
 
 def game_level():
-    level = input(f"레벨을 선택해주세요 'hard' or  easy:  ").lower
+    level = input(f"레벨을 선택해주세요 'hard' or  'easy':  ").lower
     if level == "easy" :
         return levels+5
     else: 
@@ -35,14 +35,19 @@ def play_game():
     guess = 0
     while guess != answer:
         guess = int(input("숫자 입력 : "))
-        print(f"정답을 맞추지 못해 남은 턴수가 {turns}입니다.")
         turns = check_answer(guess,answer,turns)
+        print(f"정답을 맞추지 못해 남은 턴수가 {turns}입니다.")
         
-    if turns == 0 :
-        print(f"기회가 다하여 게임에서 졌습니다.")
+        
+        if turns == 0 :
+            print(f"기회가 다하여 게임에서 졌습니다.")
+        elif turns == -1:
+            print("정답입니다 축하합니다.")
         regame = input(f"리게임을 하시겠습니까? 'y' or 'n'").lower
         if regame =='y' :
-            play_game()
+            return play_game()
+        elif guess != answer:
+            print("다시 입력하세요")
 
 play_game()      
            
