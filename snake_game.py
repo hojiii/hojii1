eff5e0a1e14bb6bfa8633973cd667f00cf64b875
hojiii <101567172+hojiii@users.noreply.github.com>
2022-04-22 01:32:38 +0900

    스네이크 게임
    class로 정리 하여 간소화

--- snake_game.py
+++ snake_game.py
@@ -1,22 +1,13 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 
 game_is_on = True
@@ -24,12 +15,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-        segments[0].forward(20)
-"""(start세그먼트,stop세그먼트,step)staring_position 순서를 대입/첫번쨰 3번쨰세그먼트가 첫번째세그먼트로 따라가게 반복문"""
+
+    snake.move()
 
 
 
